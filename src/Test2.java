21322b3b742110919229805d2ba3af6e1c005b51
Fabian <foberm16@gmail.com>
2017-09-07 11:59:03 +0200

    size fix

--- src/Test2.java
+++ src/Test2.java
@@ -16,58 +16,24 @@ import javax.swing.SwingUtilities;
 public class Test2  {
 
     public Test2() {
-       createWindow("correct opaque", false);
+       createWindow();
     }
 
-
-
-    private JLabel createLabel(final boolean opaque) {
-        JLabel but = new JLabel("Testing") {
-
-            /**
-             * @inherited <p>
-             * Overridden to take over background painting with
-             * transparent color.
-             */
-            @Override
-            protected void paintComponent(Graphics g) {
-                if (!isOpaque() && getBackground().getAlpha() < 255) {
-                    g.setColor(getBackground());
-                    g.fillRect(0, 0, getWidth(), getHeight());
-                }
-                super.paintComponent(g);
-            }
-
-        };
-        but.setBackground(new Color(0, 0, 0, 0));
-        but.setForeground(new Color(70, 155, 255));
-        but.setOpaque(opaque);
-        return but;
-    }
-    static Shoot game;
     static int x=1;
     static JWindow window;
-    private void createWindow(String text, boolean opaque) {
+    private void createWindow() {
          window = new JWindow(){
         	@Override
         	public void paint(Graphics g) {
         		super.paint(g);
-        		Graphics2D g2d = (Graphics2D) g;
-        		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-        				RenderingHints.VALUE_ANTIALIAS_ON);
-        		g.fillOval(x, 10, 30, 30);
+        		g.fillOval(x, 0, 50, 50);
 
         	}
         };
-        game = new Shoot();
-       // window.setLayout(null);
-       // window.add(game);
-        window.setOpacity(1f);
         window.setBackground(new Color(0, 0, 0, 0));
-        window.setSize(new Dimension(300, 100));
+        window.setSize(new Dimension(1920, 500));
         window.setAlwaysOnTop(true);
-
-        window.setLocation(800, 600);
+        window.setLocation(0, 1080-500);
         window.setVisible(true);
 
     }
